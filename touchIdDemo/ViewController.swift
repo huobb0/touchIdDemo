9dd260cf5fc820c47263e0df15cb090c649b2221
huobb0 <zsombor.kovacs@gmail.com>
2019-05-15 13:58:32 +0200

    compile

--- touchIdDemo/ViewController.swift
+++ touchIdDemo/ViewController.swift
@@ -7,14 +7,74 @@
 //
 
 import UIKit
+import LocalAuthentication
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var StatusLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.authenticateUsingTouchID()
     }
 
-
+    private func authenticateUsingTouchID(){
+        let context = LAContext()
+        var errorPointer: NSError? = nil
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &errorPointer){
+            NSLog("OK")
+            requestTouchIdValidation(usingContext: context)
+        } else {
+            DispatchQueue.main.async {
+                self.StatusLabel.text = "No TouchID available."
+            }
+        }
+    }
+    
+    private func requestTouchIdValidation(usingContext context: LAContext) {
+        context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: "Login Using Your Magic Touch") { (success, error) in
+            
+            if success {
+                DispatchQueue.main.async {
+                    // CONGRATULATIONS: handle success authentication =)
+                    self.StatusLabel.text = "Successfully logged in :)"
+                }
+            }
+            else {
+                let error = error as! LAError
+                
+                switch error.code {
+                case LAError.authenticationFailed:
+                    break
+                case LAError.userCancel:
+                    DispatchQueue.main.async {
+                        self.StatusLabel.text = "Canceled."
+                    }
+                    break
+                case LAError.userFallback:
+                    DispatchQueue.main.async {
+                        self.StatusLabel.text = "Fallback."
+                    }
+                    break
+                case LAError.biometryNotEnrolled:
+                    DispatchQueue.main.async {
+                        self.StatusLabel.text = "Not enrolled."
+                    }
+                    break
+                case LAError.passcodeNotSet:
+                    DispatchQueue.main.async {
+                        self.StatusLabel.text = "Passcode not set."
+                    }
+                    break
+                case LAError.systemCancel:
+                    // canceled by the system
+                    break
+                default:
+                    // other
+                    break
+                }
+            }
+        }
+    }
 }
 
